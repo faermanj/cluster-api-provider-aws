a8165b9bbc6ed99b6e1e374abe230eefa3a4df87
Julio Faerman <jufaerma@redhat.com>
2024-11-13 14:32:32 +0000

    wip

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,6 +6,13 @@ kind --version
 kubectl version --client
 docker --version
 
+echo "Setup docker in docker"
+curl https://raw.githubusercontent.com/faermanj/cluster-api-provider-aws/refs/heads/add-dedicated-hosts/.devcontainer/docker-in-docker-debian.sh | bash
+
 echo "Verifying system"
+whoami
 newgrp docker || true
 docker ps
+pwd
+find .
+
