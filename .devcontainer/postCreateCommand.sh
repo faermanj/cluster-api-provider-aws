25fae2321c97a8c62c5713bf711ffee99549f09e
Julio Faerman <jufaerma@redhat.com>
2024-11-13 13:39:51 +0100

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,7 +3,11 @@
 echo "Starting Docker"
 sudo service docker start
 
-echo "Verifying Versions"
+echo "Verifying versions"
 go version
 kind --version
 kubectl version --client
+docker --version
+
+echo "Verifying system"
+docker ps
